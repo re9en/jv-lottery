251d163a22040cbdad3ec4163b819ea16fd360c6
Timofii Kniahnytskiy <regen69neger@gmail.com>
2025-11-11 03:29:31 +0200

    Added new classes, new methods

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,7 +1,11 @@
 package core.basesyntax;
 
+import java.util.Random;
+
 public class ColorSupplier {
     public String getRandomColor() {
-        return null;
+        int randomIndex = new Random().nextInt(Colors.values().length);
+        Colors colors = Colors.values()[randomIndex];
+        return colors.toString();
     }
 }
